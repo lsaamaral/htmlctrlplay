214e46a0f8af5d3124092a226ba4c9d4cb5305ed
Isabella Amaral <isabellaamaral@outlook.pt>
2025-11-18 14:24:37 -0300

    client arquivos

--- CY4/pokedex/client/src/App.jsx
+++ CY4/pokedex/client/src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PokemonList from './pages/pokemonList';
 import PokemonForm from '../components/pokemonForm';
